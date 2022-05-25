d49de53fd30f8a09948e6073ac33f61927345492
ledamorita <101803443+ledamorita@users.noreply.github.com>
2022-05-25 23:34:58 +0900

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import db from "./database";
 import ResponseObj from "./utils";
 const app: express.Express = express();
-const PORT = 5000;
+const PORT = 9000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
